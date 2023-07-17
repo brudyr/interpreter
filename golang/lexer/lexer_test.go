d0891df1ea42ab8faf7d0da1b3f02f9d4b866d75
brudyr <ron@brudy.dev>
2023-07-17 14:08:03 +0200

    Add tokenTypes and tests for advanced tokens

--- golang/lexer/lexer_test.go
+++ golang/lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/brudyr/go-interpreter/token"
 )
 
-func TestNextToken(t *testing.T) {
+func TestSingleCharTokens(t *testing.T) {
   input := "(){}+-=,;"
 
   testCases := []struct {
@@ -25,6 +25,74 @@ func TestNextToken(t *testing.T) {
     { token.EOF, "" },
   }
   
+  l := New(input)
+
+  for index, testCase := range testCases {
+
+    currentToken := l.NextToken();
+
+    if currentToken.Type != testCase.expectedType {
+      t.Fatalf(
+        "[testcase #%d] - Wrong TokenType - Expected: '%s' | Got: '%s'",
+        index,
+        currentToken.Type, testCase.expectedType,
+      )
+    }
+  }
+}
+
+func TestMultiCharTokens(t *testing.T) {
+  input := `let someNum = 6;
+  let biggerNum = 12;
+
+  let add = fn(a, b) {
+    a + b
+  }
+
+  let sum = add(someNum, biggerNum);
+  `
+
+  testCases := []struct {
+    expectedType token.TokenType
+    expectedVal string
+  }{
+    { token.LET, "let" },
+    { token.IDENT, "someNum" },
+    { token.ASSIGN, "=" },
+    { token.INT, "6" },
+    { token.SEMICOLON, ";" },
+    { token.LET, "let" },
+    { token.IDENT, "biggerNum" },
+    { token.ASSIGN, "=" },
+    { token.INT, "12" },
+    { token.SEMICOLON, ";" },
+    { token.LET, "let" },
+    { token.IDENT, "add" },
+    { token.ASSIGN, "=" },
+    { token.FUNCTION, "fn" },
+    { token.LPAREN, "(" },
+    { token.IDENT, "a" },
+    { token.COMMA, "," },
+    { token.IDENT, "b" },
+    { token.RPAREN, ")" },
+    { token.LBRACE, "{" },
+    { token.IDENT, "a" },
+    { token.PLUS, "+" },
+    { token.IDENT, "b" },
+    { token.RBRACE, "}" },
+    { token.LET, "let" },
+    { token.IDENT, "sum" },
+    { token.ASSIGN, "=" },
+    { token.IDENT, "add" },
+    { token.LPAREN, "(" },
+    { token.IDENT, "someNum" },
+    { token.COMMA, "," },
+    { token.IDENT, "biggerNum" },
+    { token.RPAREN, ")" },
+    { token.SEMICOLON, ";" },
+    { token.EOF, "" },
+  }
+
   l := New(input)
 
   for index, testCase := range testCases {
--- golang/token/token.go
+++ golang/token/token.go
@@ -14,6 +14,10 @@ const (
   MINUS = "-"
   COMMA = ","
   SEMICOLON = ";"
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+  IDENT = "IDENT"
+  INT = "INT"
 )
 
 type Token struct {
