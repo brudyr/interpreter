360e7224360137679b158d943481481901b90b8e
brudyr <ron@brudy.dev>
2023-08-08 11:14:51 +0200

    Implement basic parsing of let statements

--- golang/parser/parser.go
+++ golang/parser/parser.go
@@ -29,5 +29,61 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-  return nil
+  program := &ast.Program{}
+  program.Statements = []ast.Statement{}
+
+  for p.curToken.Type != token.EOF {
+    statement := p.parseStatement()
+    if statement != nil {
+      program.Statements = append(program.Statements, statement)
+    }
+    p.nextToken()
+  }
+  return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+  switch p.curToken.Type {
+    case token.LET:
+      return p.parseLetStatement()
+    default:
+      return nil
+  }
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+  statement := &ast.LetStatement{Token: p.curToken}
+
+  if !p.expectPeek(token.IDENT) {
+    return nil
+  }
+
+  statement.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Value}
+
+  if !p.expectPeek(token.ASSIGN) {
+    return nil
+  }
+
+  // skip until semicolon
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+  return statement
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+  return p.curToken.Type == t
+}
+
+func (p *Parser) peektTokenIs(t token.TokenType) bool {
+  return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+  if p.peektTokenIs(t) {
+    p.nextToken()
+    return true
+  } else {
+    return false
+  }
 }
