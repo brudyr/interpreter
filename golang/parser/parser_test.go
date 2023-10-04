97784d7f5f8fcf56920b991df27d747e45f4ac00
brudyr <ron@brudy.dev>
2023-10-04 13:55:14 +0200

    Add precedence test

--- golang/parser/parser_test.go
+++ golang/parser/parser_test.go
@@ -287,3 +287,71 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	}{
+	  {
+	    "-a * b",
+      "((-a) * b)",
+    },
+    {
+      "!-a",
+      "(!(-a))",
+    },
+    {
+      "a + b + c",
+      "((a + b) + c)",
+    },
+    {
+      "a + b - c",
+      "((a + b) - c)",
+    },
+    {
+      "a * b * c",
+      "((a * b) * c)",
+    },
+    {
+      "a * b / c",
+      "((a * b) / c)",
+    },
+    {
+      "a + b / c",
+      "(a + (b / c))",
+    },
+    {
+      "a + b * c + d / e - f",
+      "(((a + (b * c)) + (d / e)) - f)",
+    },
+    {
+      "3 + 4; -5 * 5",
+      "(3 + 4)((-5) * 5)",
+    },
+    {
+      "5 > 4 == 3 < 4",
+      "((5 > 4) == (3 < 4))",
+    },
+    {
+      "5 < 4 != 3 > 4",
+      "((5 < 4) != (3 > 4))",
+    },
+    {
+      "3 + 4 * 5 == 3 * 1 + 4 * 5",
+      "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+    },
+	}
+
+	for _, testCase := range tests {
+		lexer := lexer.New(testCase.input)
+		parser := New(lexer)
+		program := parser.ParseProgram()
+		checkParserErrors(t, parser)
+
+		actual := program.String()
+		if actual != testCase.expected {
+			t.Errorf("Expected=%q, got%q", testCase.expected, actual)
+		}
+	}
+}
